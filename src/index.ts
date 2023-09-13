3396610d7566195ba15d478c61361e34dcc72760
Viniciuslb067 <viniciuslb067@gmail.com>
2023-09-13 11:05:33 -0300

    refactor: change routes name

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import routes from "./routes";
+import employeeRoutes from "./routes/employee-route";
 
 const app = express();
 
 app.use(express.json());
 
-app.use(routes);
+app.use(employeeRoutes);
 
 const PORT = process.env.PORT ?? 3000;
 
